4496182e43b0d825110f6fab1a34ea1ec2211fe5
CFT716 <417278668@qq.com>
2020-07-17 15:34:59 +0800

    Test1又添加一句输出语句

--- src/Text1.java
+++ src/Text1.java
@@ -3,18 +3,19 @@ import java.util.Scanner;
 public class Text1 {
 
 	public static void main(String[] args) {
-		System.out.println("请输入年份:");
-		System.out.println("请输入年份:");
-        int year;//接收输入的年份
+		System.out.println("璇疯緭鍏ュ勾浠�:");
+		System.out.println("璇疯緭鍏ュ勾浠�:");
+		System.out.println("闰年输出：");
+        int year;//鎺ユ敹杈撳叆鐨勫勾浠�
         Scanner scanner = new Scanner(System.in);
         year = scanner.nextInt();
         if(year<0||year>3000){
-            System.out.println("年份有误！");
+            System.out.println("骞翠唤鏈夎锛�");
         }
         if(year%4==0&&year%100!=0||year%400==0){
-            System.out.println(year+"是闰年");
+            System.out.println(year+"鏄棸骞�");
         }else{
-            System.out.println(year+"不是闰年");
+            System.out.println(year+"涓嶆槸闂板勾");
         }
     }
 }
