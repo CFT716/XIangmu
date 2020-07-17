2de5daaffa19a643e516dd5855ea42fb7968679d
CFT716 <417278668@qq.com>
2020-07-17 15:48:15 +0800

    Test1文件进行了修改操作

--- src/Text1.java
+++ src/Text1.java
@@ -5,7 +5,7 @@ public class Text1 {
 	public static void main(String[] args) {
 		System.out.println("璇疯緭鍏ュ勾浠�:");
 		System.out.println("璇疯緭鍏ュ勾浠�:");
-		System.out.println("闰年输出：");
+		System.out.println("闰年输出");
         int year;//鎺ユ敹杈撳叆鐨勫勾浠�
         Scanner scanner = new Scanner(System.in);
         year = scanner.nextInt();
